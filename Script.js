1873084a6bb765adb921906a1b400db15aa7d2e9
habiblimon <md.habiblimon94@gmail.com>
2023-12-17 23:59:21 +0600

    LWS-Javascript

---
+++ Script.js
@@ -0,0 +1,32 @@
+/*Introduction To javaScript
+ ==========================
+>> 1995 (MR. Brendan Eich)
+>> ECMA = Europian Computer manufacrus Assosion (1997)
+
+>> ES 7 8 9
+>> JavaSript can change HTML Attribute .
+>> JavaSript can change HTML Style.
+>> fontSize == 
+>> JavaScript can Show HTML Element.
+>> where To <script> </script>
+>> Head OR Body Can write .
+>> Extarnal file (something.js) 
+
+JavaScript Output
+==================
+
+>> JavaScript Display Possibilites
+>> getElementById = Camal Case 
+>> document.write();
+>> consol.log();
+>> window.alert(); / alert();
+>> window.print();
+
+
+
+
+
+
+*/
+
+
